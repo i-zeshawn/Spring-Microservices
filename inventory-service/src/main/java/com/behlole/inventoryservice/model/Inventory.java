130c0f7c54a2084cc979ff420e09be9f5b2248c9
i-zeshawn <zeeshahmed.dev@gmail.com>
2023-07-24 21:09:54 -0400

    updated inventory model

--- inventory-service/src/main/java/com/behlole/inventoryservice/model/Inventory.java
+++ inventory-service/src/main/java/com/behlole/inventoryservice/model/Inventory.java
@@ -1,4 +1,18 @@
 package com.behlole.inventoryservice.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "t_inventory")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String skuCode;
+    private Integer quantity;
+
 }
