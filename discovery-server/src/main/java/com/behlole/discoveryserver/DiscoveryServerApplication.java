41087bfd5c8c02dc8e7bcb83d1bcb57fd5d1d773
i-zeshawn <zeeshahmed.dev@gmail.com>
2023-07-26 01:08:40 -0400

    implemented basic Eureka Server

--- discovery-server/src/main/java/com/behlole/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/behlole/discoveryserver/DiscoveryServerApplication.java
@@ -1,18 +1,14 @@
 package com.behlole.discoveryserver;
 
-import com.netflix.discovery.shared.Application;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
 @EnableEurekaServer
-@EnableDiscoveryClient
 public class DiscoveryServerApplication {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(DiscoveryServerApplication.class).run(args);
+        SpringApplication.run(DiscoveryServerApplication.class, args);
     }
 }
